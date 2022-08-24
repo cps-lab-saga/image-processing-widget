40a29b097c15b03b931e193f2e49e6a335d2d207
wenliangyeoh <wlyeoh@cc.saga-u.ac.jp>
2022-08-24 20:28:20 +0900

    add no processing option

---
+++ plugins/none.py
@@ -0,0 +1,15 @@
+from image_processing_widget.defs import QtWidgets
+from image_processing_widget.plugin_objects import ProcessPlugin
+
+
+class DoNothing(ProcessPlugin):
+    def __init__(self):
+        super().__init__()
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    widget = DoNothing()
+    widget.show()
+
+    app.exec()
