1e1896522457d77f312f279711af9011d1d6c4d8
wenliangyeoh <wlyeoh@cc.saga-u.ac.jp>
2023-01-20 17:41:32 +0900

    fix blob detector

--- plugins/blob_detect.py
+++ plugins/blob_detect.py
@@ -17,45 +17,49 @@ class BlobDetect(ProcessPlugin):
         self.threshold_range_control.setValue(0, 2**8 - 1)
         self.form_layout.addRow("Threshold:", self.threshold_range_control)
 
-        self.area_control = SpinBoxSlider(decimals=0, orientation=QtCore.Qt.Horizontal)
-        self.area_control.setSingleStep(1)
-        self.area_control.setRange(0, 100)
-        self.form_layout.addRow("Area:", self.area_control)
+        self.area_range_control = SpinBoxRangeSlider(
+            decimals=0, orientation=QtCore.Qt.Horizontal
+        )
+        self.area_range_control.setSingleStep(1)
+        self.area_range_control.setRange(1, 100)
+        self.form_layout.addRow("Area:", self.area_range_control)
 
-        self.circularity_control = SpinBoxSlider(
+        self.circularity_range_control = SpinBoxRangeSlider(
             decimals=2, orientation=QtCore.Qt.Horizontal
         )
-        self.circularity_control.setSingleStep(0.01)
-        self.circularity_control.setRange(0, 1)
-        self.form_layout.addRow("Circularity:", self.circularity_control)
+        self.circularity_range_control.setSingleStep(0.01)
+        self.circularity_range_control.setRange(0.01, 1)
+        self.form_layout.addRow("Circularity:", self.circularity_range_control)
 
-        self.convexity_control = SpinBoxSlider(
+        self.convexity_range_control = SpinBoxRangeSlider(
             decimals=2, orientation=QtCore.Qt.Horizontal
         )
-        self.convexity_control.setSingleStep(0.01)
-        self.convexity_control.setRange(0, 1)
-        self.form_layout.addRow("Convexity:", self.convexity_control)
+        self.convexity_range_control.setSingleStep(0.01)
+        self.convexity_range_control.setRange(0.01, 1)
+        self.form_layout.addRow("Convexity:", self.convexity_range_control)
 
-        self.inertia_control = SpinBoxSlider(
+        self.inertia_range_control = SpinBoxRangeSlider(
             decimals=2, orientation=QtCore.Qt.Horizontal
         )
-        self.inertia_control.setSingleStep(0.01)
-        self.inertia_control.setRange(0, 1)
-        self.form_layout.addRow("Inertia:", self.inertia_control)
+        self.inertia_range_control.setSingleStep(0.01)
+        self.inertia_range_control.setRange(0.01, 1)
+        self.form_layout.addRow("Inertia Ratio:", self.inertia_range_control)
 
         self.threshold_range_control.valueChanged.connect(
             lambda _: self.settings_updated.emit()
         )
-        self.area_control.valueChanged.connect(lambda _: self.settings_updated.emit())
-        self.circularity_control.valueChanged.connect(
+        self.area_range_control.valueChanged.connect(
             lambda _: self.settings_updated.emit()
         )
-        self.inertia_control.valueChanged.connect(
+        self.circularity_range_control.valueChanged.connect(
+            lambda _: self.settings_updated.emit()
+        )
+        self.inertia_range_control.valueChanged.connect(
             lambda _: self.settings_updated.emit()
         )
 
     def adjust_range(self, shape):
-        self.area_control.setRange(0, shape[0] * shape[1] / 4)
+        self.area_range_control.setRange(1, shape[0] * shape[1] / 4)
 
     def process_img(self, img):
         detector = self.setup_blob_detector()
@@ -74,10 +78,10 @@ class BlobDetect(ProcessPlugin):
 
     def setup_blob_detector(self):
         min_thresh, max_thresh = self.threshold_range_control.value()
-        area = self.area_control.value()
-        circularity = self.circularity_control.value()
-        convexity = self.convexity_control.value()
-        inertia = self.inertia_control.value()
+        min_area, max_area = self.area_range_control.value()
+        min_circularity, max_circularity = self.circularity_range_control.value()
+        min_convexity, max_convexity = self.convexity_range_control.value()
+        min_inertia, max_inertia = self.inertia_range_control.value()
 
         params = cv.SimpleBlobDetector_Params()
 
@@ -85,16 +89,20 @@ class BlobDetect(ProcessPlugin):
         params.maxThreshold = max_thresh
 
         params.filterByArea = True
-        params.minArea = area
+        params.minArea = min_area
+        params.maxArea = max_area
 
         params.filterByCircularity = True
-        params.minCircularity = circularity
+        params.minCircularity = min_circularity
+        params.maxCircularity = max_circularity
 
         params.filterByConvexity = True
-        params.minConvexity = convexity
+        params.minConvexity = min_convexity
+        params.maxConvexity = max_convexity
 
         params.filterByInertia = True
-        params.minInertiaRatio = inertia
+        params.minInertiaRatio = min_inertia
+        params.maxInertiaRatio = max_inertia
 
         return cv.SimpleBlobDetector_create(params)
 
